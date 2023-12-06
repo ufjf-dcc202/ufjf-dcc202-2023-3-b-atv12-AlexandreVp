94e7835d9a0b54af93841364436b1cadb6c78ea9
Alexandre <alexandre.vieira@estudante.ufjf.br>
2023-12-05 21:40:44 -0300

    Criando estoque.js e função getEstoque

---
+++ estoque.js
@@ -0,0 +1,16 @@
+let estoque = {
+    'joao': [
+        {'tipo': 'maca', 'qtd': 1},
+        {'tipo': 'pera', 'qtd': 2}
+    ],
+    'maria': [
+        {'tipo': 'maca', 'qtd': 2},
+        {'tipo': 'pera', 'qtd': 4}
+    ]
+}
+
+function getEstoque(){
+    return structuredClone(estoque)
+}
+
+export {getEstoque}
